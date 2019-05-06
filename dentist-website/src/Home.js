bb3d4829f496dd2327ea685731058bc965dbc8a0
MuhammadHakami <engmuhammadhakami@gmail.com>
2019-05-06 11:02:14 +0300

    built the back-bone

---
+++ dentist-website/src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+        <div>
+        <h1>Dentist Website</h1>
+        <p>
+          Welcome to the dentist office.
+        </p>
+      </div>
+    )
+  }
+}
